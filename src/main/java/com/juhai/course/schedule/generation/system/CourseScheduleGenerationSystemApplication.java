4c98d0cdf5c201b4ac2c0db1029dfe12c954eed1
juhai <juhai404@outlook.com>
2025-05-13 16:20:06 +0800

    init

---
+++ src/main/java/com/juhai/course/schedule/generation/system/CourseScheduleGenerationSystemApplication.java
@@ -0,0 +1,18 @@
+package com.juhai.course.schedule.generation.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <h2> 启动类 </h2>
+ *
+ * @author juhai
+ * date 2025/5/13
+ */
+@SpringBootApplication
+public class CourseScheduleGenerationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseScheduleGenerationSystemApplication.class, args);
+    }
+}
